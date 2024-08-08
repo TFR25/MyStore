b4e2425cc6b274fed3ae02ef4daf22cd4638c2a7
Tasha Fernandez-Ross <tfernandez25@gmail.com>
2024-08-08 05:44:55 -0700

    add products and router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">
-          <img class="logo" src={logo} alt="Naturely Gifts" />
-            Naturely Gifts
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img class="logo" src={logo} alt="Naturely Gifts" />
+              Naturely Gifts
+            </Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link hreg="/cart">
-                <FaShoppingCart /> Cart{" "}
-              </Nav.Link>
-              <Nav.Link hreg="/login">
-                <FaUser /> Sign In{" "}
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart{" "}
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In{" "}
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
