fd827964d80037afd49a0b1758683820d32253b7
Tasha Fernandez-Ross <tfernandez25@gmail.com>
2024-08-13 05:30:27 -0700

    Set up server

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Gravity Ceramic mug",
+    image: "/images/GravityMug.png",
+    description:
+      "Warm-up with a nice cuppa out of this customized ceramic coffee mug. Personalize it with cool designs, photos or logos to make that 'aaahhh!' moment even better. It's BPA and Lead-free, microwave &amp; dishwasher-safe, and made of white, durable ceramic in 11-ounce and 15-ounce sizes. Thanks to the advanced printing tech, your designs come to life with incredibly vivid colors, the perfect gift for coffee, tea, and chocolate lovers.",
+    brand: "Naturely Gifts",
+    category: "Coffee Mug",
+    price: 7.68,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Fishing Fanatic Mug",
+    image: "/images/FishingFanatic.png",
+    description:
+      "Warm-up with a nice cuppa out of this customized ceramic coffee mug. Personalize it with cool designs, photos or logos to make that 'aaahhh!' moment even better. It's BPA and Lead-free, microwave &amp; dishwasher-safe, and made of white, durable ceramic in 11-ounce and 15-ounce sizes. Thanks to the advanced printing tech, your designs come to life with incredibly vivid colors, the perfect gift for coffee, tea, and chocolate lovers.",
+    brand: "Naturely Gifts",
+    category: "Coffee Mug",
+    price: 7.68,
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Premium Crewneck Fishermen T-shirt for Nature and Fishermen Lovers",
+    image: "/images/FishingTee.png",
+    description:
+      "Dive into the tranquility of Canon Beach with our Premium Unisex Crewneck Fishermen T-shirt for Nature and Fishermen Lovers. Crafted for nature and fisherman enthusiasts, this nature lovers t-shirt showcases a vivid scene of a fisherman in action, bringing the serene beauty of Oregon’s coastline to your wardrobe. Made from the softest 100% Airlume combed and ring-spun cotton, it ensures all-day comfort. Whether you’re casting a line or simply enjoying the view, this eco-friendly tee is your go-to for both style and sustainability.",
+    brand: "Naturely Gifts",
+    category: "T-Shirt",
+    price: 23.0,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Eco-conscious Polyester Fishermen Pullover Hoodie for Nature and Fishermen Lovers",
+    image: "/images/FishingHoodie.png",
+    description:
+      "Embrace sustainability with this cozy, eco-conscious hoodie, perfect for nature lovers who care about the planet.",
+    brand: "Naturely Gifts",
+    category: "Hoodies",
+    price: 66.38,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Eco-conscious Polyester Unisex Nature Lovers Pullover Hoodie",
+    image: "/images/UpsideDownHoodie.png",
+    description:
+      "Celebrate nature with our EcoPerch Hoodie, featuring a stunning yellow-eyed warbler perched upside down from a palm frond. This unisex pullover hoodie is crafted from 80% organically grown cotton and 20% recycled polyester, combining sustainability with comfort. With a 9 oz. density, it’s perfect for all settings, whether you're exploring the outdoors or cozying up at home. The double-needle cover-stitched seams ensure lasting durability, while the cotton-lined hood adds an extra layer of comfort. Wear your love for nature proudly with this eco-friendly, conservation-focused hoodie.",
+    brand: "Naturely Gifts",
+    category: "Hoodie",
+    price: 66.38,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Eco-conscious Polyester Unisex Nature Lovers T-Shirt",
+    image: "/images/UpsideDownTee.png",
+    description:
+      "Discover effortless elegance with this micro ribbed tank, perfect for nature lovers who value style and comfort in every adventure.",
+    brand: "Naturely Gifts",
+    category: "T-Shirts",
+    price: 34.42,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
