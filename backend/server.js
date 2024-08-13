6a9318408602dbfe155181dd47bb8d2d04d0b375
Tasha Fernandez-Ross <tfernandez25@gmail.com>
2024-08-13 08:40:57 -0700

    set up and connect MongoDB project

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
@@ -16,9 +18,9 @@ app.get("/api/products", (req, res) => {
 
 app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
-  res.json(products);
+  res.json(product);
 });
 
-app.gert;
+app.get;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
-import products from "../products";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Rating from "../components/Rating";
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [productId]);
+
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
@@ -47,14 +56,22 @@ const ProductScreen = () => {
                 <Row>
                   <Col>Status:</Col>
                   <Col>
-                    <strong>{product.countInStock > 0 ? "In Stock" :
-                        "Out of Stock"}</strong>
+                    <strong>
+                      {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                    </strong>
                   </Col>
                 </Row>
               </ListGroup.Item>
-            <ListGroup.Item>
-                <Button className="btn btn-block" type="button" disabled={product.countInStock === 0}> Add To Cart</Button>
-            </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  {" "}
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
             </ListGroup>
           </Card>
         </Col>
